552c6827fd0318854d6fe1b7c6168a40b4c0a6ca
Georgi Parlakov <gparlakov@gmail.com>
2019-07-06 15:34:37 +0300

    refactor two main functions create and update

--- src/spec/index.ts
+++ src/spec/index.ts
@@ -7,7 +7,8 @@ import {
     SchematicContext,
     template,
     Tree,
-    url
+    url,
+    FileEntry
 } from '@angular-devkit/schematics';
 import { EOL } from 'os';
 import { Change, InsertChange, RemoveChange } from '../../lib/utility/change';
@@ -40,94 +41,110 @@ export function spec({ name, update: up }: SpecOptions): Rule {
         const specFileFullName = `${normalizedName}.spec.ts`;
 
         const existingSpecFile = tree.get(specFileFullName);
-        // if a spec exists we'll update it
+
+        // if a spec exists we'll try to update it
         if (existingSpecFile) {
-            if (up !== true) {
-                throw new Error(
-                    'The spec file already exists. Please specify --update or -u if you want to update the spec file.'
-                );
-            }
-            const { params, className } = parseClassUnderTestFile(name, content);
-            const removeChanges = update(
-                existingSpecFile.path,
-                existingSpecFile.content.toString('utf8'),
-                params,
-                className,
-                'remove'
-            );
-
-            // first pass - remove
-            const removeRecorder = tree.beginUpdate(existingSpecFile.path);
-            removeChanges.forEach((change: Change) => {
-                if (change instanceof RemoveChange) {
-                    removeRecorder.remove(change.order, change.toRemove.length);
-                }
-            });
-            tree.commitUpdate(removeRecorder);
-
-            // now we need to redo the update on the new file contents
-            const changesToAdd = update(
-                existingSpecFile.path,
-                tree.read(existingSpecFile.path)!.toString('utf8'),
-                params,
-                className,
-                'add'
-            );
-
-            // first pass - add
-            const addRecorder = tree.beginUpdate(existingSpecFile.path);
-            changesToAdd.forEach((change: Change) => {
-                if (change instanceof InsertChange) {
-                    addRecorder.insertLeft(change.order, change.toAdd);
-                }
-            });
-            tree.commitUpdate(addRecorder);
-
-            return tree;
+            return updateExistingSpec(up, name, content, existingSpecFile, tree);
         } else {
             // spec file does not exist
-            const { params, className, publicMethods } = parseClassUnderTestFile(name, content);
-            const templateSource = apply(url('../files'), [
-                template({
-                    // the name of the new spec file
-                    specFileFullName,
-                    normalizedName: normalizedName,
-                    className: className,
-                    publicMethods,
-                    declaration: toDeclaration(),
-                    builderExports: toBuilderExports(),
-                    constructorParams: toConstructorParams()
-                })
-            ]);
-            return branchAndMerge(mergeWith(templateSource))(tree, context);
-
-            // functions defined in the scope of the else to use params and such
-            // for getting called in the template - todo - just call the functions and get the result
-            function toConstructorParams() {
-                return params.map(p => p.name).join(',');
-            }
-
-            function toDeclaration() {
-                return params
-                    .map(p =>
-                        p.type === 'string' || p.type === 'number'
-                            ? `let ${p.name}:${p.type};`
-                            : `const ${p.name} = autoSpy(${p.type});`
-                    )
-                    .join(EOL);
-            }
-
-            function toBuilderExports() {
-                return params.length > 0
-                    ? params
-                          .map(p => p.name)
-                          .join(',' + EOL)
-                          .concat(',')
-                    : '';
-            }
+            return createNewSpec(name, content, specFileFullName, normalizedName, tree, context);
         }
     };
 }
+
+function updateExistingSpec(
+    up: boolean,
+    name: string,
+    content: Buffer,
+    existingSpecFile: FileEntry,
+    tree: Tree
+) {
+    if (up !== true) {
+        throw new Error(
+            'The spec file already exists. Please specify --update or -u if you want to update the spec file.'
+        );
+    }
+    const { params, className } = parseClassUnderTestFile(name, content);
+    const removeChanges = update(
+        existingSpecFile.path,
+        existingSpecFile.content.toString('utf8'),
+        params,
+        className,
+        'remove'
+    );
+    // first pass - remove
+    const removeRecorder = tree.beginUpdate(existingSpecFile.path);
+    removeChanges.forEach((change: Change) => {
+        if (change instanceof RemoveChange) {
+            removeRecorder.remove(change.order, change.toRemove.length);
+        }
+    });
+    tree.commitUpdate(removeRecorder);
+    // now we need to redo the update on the new file contents
+    const changesToAdd = update(
+        existingSpecFile.path,
+        tree.read(existingSpecFile.path)!.toString('utf8'),
+        params,
+        className,
+        'add'
+    );
+    // first pass - add
+    const addRecorder = tree.beginUpdate(existingSpecFile.path);
+    changesToAdd.forEach((change: Change) => {
+        if (change instanceof InsertChange) {
+            addRecorder.insertLeft(change.order, change.toAdd);
+        }
+    });
+    tree.commitUpdate(addRecorder);
+    return tree;
+}
+
+function createNewSpec(
+    name: string,
+    content: Buffer,
+    specFileFullName: string,
+    normalizedName: string,
+    tree: Tree,
+    context: SchematicContext
+) {
+    const { params, className, publicMethods } = parseClassUnderTestFile(name, content);
+    const templateSource = apply(url('../files'), [
+        template({
+            // the name of the new spec file
+            specFileFullName,
+            normalizedName: normalizedName,
+            className: className,
+            publicMethods,
+            declaration: toDeclaration(),
+            builderExports: toBuilderExports(),
+            constructorParams: toConstructorParams()
+        })
+    ]);
+    return branchAndMerge(mergeWith(templateSource))(tree, context);
+    // functions defined in the scope of the else to use params and such
+    // for getting called in the template - todo - just call the functions and get the result
+    function toConstructorParams() {
+        return params.map(p => p.name).join(',');
+    }
+    function toDeclaration() {
+        return params
+            .map(p =>
+                p.type === 'string' || p.type === 'number'
+                    ? `let ${p.name}:${p.type};`
+                    : `const ${p.name} = autoSpy(${p.type});`
+            )
+            .join(EOL);
+    }
+    function toBuilderExports() {
+        return params.length > 0
+            ? params
+                  .map(p => p.name)
+                  .join(',' + EOL)
+                  .concat(',')
+            : '';
+    }
+}
+
 
 function parseClassUnderTestFile(name: string, fileContents: Buffer) {
     const classDescriptions = readClassNamesAndConstructorParams(
