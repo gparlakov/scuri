fb9b6dbf57fc2d468242e460f151acba2db166fc
Georgi Parlakov <gparlakov@gmail.com>
2019-08-07 21:28:22 +0300

    m

--- example/angular-5-app/src/app/my-com/my-com.component.spec.ts
+++ example/angular-5-app/src/app/my-com/my-com.component.spec.ts
@@ -3,30 +3,28 @@ import { MyComComponent } from './my-com.component';
 import { autoSpy } from 'autoSpy';
 
 describe('MyComComponent', () => {
-  it('when ngOnInit is called it should', () => {
-    // arrange
-    const { build } = setup().default();
-    const c = build();
-    // act
-    c.ngOnInit();
-    // assert
-    // expect(c).toEqual
-  });
-
-
+    it('when ngOnInit is called it should', () => {
+        // arrange
+        const { build } = setup().default();
+        const c = build();
+        // act
+        c.ngOnInit();
+        // assert
+        // expect(c).toEqual
+    });
 });
 
 function setup() {
-  const router = autoSpy(Router);
-  const builder = {
-    router,
-    default() {
-      return builder;
-    },
-    build() {
-      return new MyComComponent(router);
-    }
-  };
+    const router = autoSpy(Router);
+    const builder = {
+        router,
+        default() {
+            return builder;
+        },
+        build() {
+            return new MyComComponent(router);
+        }
+    };
 
-  return builder;
+    return builder;
 }
