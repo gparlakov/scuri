90012bf115b13d7862269089307557fde5d71fad
Georgi Parlakov <gparlakov@gmail.com>
2019-08-08 22:05:09 +0300

    update after pretty-quick

--- src/autospy/files/jasmine/auto-spy.ts
+++ src/autospy/files/jasmine/auto-spy.ts
@@ -1,16 +1,16 @@
 /** Keeps the types of properties of a type but assigns type of Spy to the methods */
 type SpyOf<T> = Partial<
-  { [k in keyof T]: T[k] extends (...args: any[]) => any ? jasmine.Spy : T[k] }
+    { [k in keyof T]: T[k] extends (...args: any[]) => any ? jasmine.Spy : T[k] }
 > &
-  T;
+    T;
 
 /** Create an object with methods that are autoSpy-ed to use as mock dependency */
 function autoSpy<T>(obj: new (...args: any[]) => T): SpyOf<T> {
-  const res: SpyOf<T> = {} as any;
+    const res: SpyOf<T> = {} as any;
 
-  Object.keys(obj.prototype).forEach(key => {
-    res[key] = jasmine.createSpy(key);
-  });
+    Object.keys(obj.prototype).forEach(key => {
+        res[key] = jasmine.createSpy(key);
+    });
 
-  return res;
+    return res;
 }
--- src/autospy/files/jest/auto-spy.ts
+++ src/autospy/files/jest/auto-spy.ts
@@ -1,16 +1,16 @@
 /** Keeps the types of properties of a type but assigns type of Spy to the methods */
 type SpyOf<T> = Partial<
-  { [k in keyof T]: T[k] extends (...args: any[]) => any ? jest.Mock<T[R]> : T[k] }
+    { [k in keyof T]: T[k] extends (...args: any[]) => any ? jest.Mock<T[R]> : T[k] }
 > &
-  T;
+    T;
 
 /** Create an object with methods that are autoSpy-ed to use as mock dependency */
 function autoSpy<T>(obj: new (...args: any[]) => T): SpyOf<T> {
-  const res: SpyOf<T> = {} as any;
+    const res: SpyOf<T> = {} as any;
 
-  Object.keys(obj.prototype).forEach(key => {
-    res[key] = jest.fn();
-  });
+    Object.keys(obj.prototype).forEach(key => {
+        res[key] = jest.fn();
+    });
 
-  return res;
+    return res;
 }
