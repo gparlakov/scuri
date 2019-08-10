8938648a4df9a917a6ee6bd970de24b3860946c6
Georgi Parlakov <gparlakov@gmail.com>
2019-08-10 21:48:45 +0300

    Add angular 6 app

---
+++ example/angular-6-app/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { Service } from "./service";
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule],
+    providers: [Service],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
