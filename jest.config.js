72087a12ce2cc9cd6caa022d3488269398aad49b
Georgi Parlakov <gparlakov@gmail.com>
2020-03-19 17:42:20 +0200

    Switch to jest and rename spec files (#69)
    * Switch to jest and rename spec files
    
    * add autospy spec too and reinclude @types/jasmine
    
    * push jest.conig.js
    
    * include jest config
    
    * prettified files

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+// https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
+module.exports = {
+    globals: {
+        'ts-jest': {
+            tsConfig: '<rootDir>/tsconfig.test.json'
+        }
+    },
+    transform: {
+        '^.+\\.(ts)$': 'ts-jest'
+    },
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1'
+    },
+    transformIgnorePatterns: ['node_modules/(?!@ngrx)', 'node_modules/(?!(jest-test))'],
+    roots: ['tests']
+};
