34c78a40381c43ca024e65ce0df5516c70cb4009
Georgi Parlakov <gparlakov@gmail.com>
2022-08-14 22:55:14 +0300

    Feat/test dist with ng 14 container from scuri examples (#157)
    * chore: bump version
    
    * feat: live test with container
    
    * feat: add live test
    
    - live-test.sh
      - packs the npm package locally in a tarball and copies it into the ./example folder
      - gets the workdir of the image
      - starts the docker container and passes in the /example folder (as volume) in the angular app folder
      - supports DEBUG_LIVE_TEST for live debugging of the container
      - runs ./example/run-plus.sh in the workdir of the started container
    - run-plus.sh
      - installs the local scuri via the tarbal
      - updates karma.conf.js for running chrome headless
      - creates autospy (and adds it to the tsconfg.json path)
      - creates my-com.component.ts using a class template
      - updates one component using a class template function
      -  creates other.component with an observable and promise method
      -  add a jest suite to snapshot and test the results
    
    * echo docker
    
    * debug pipelines
    
    * debug azdo: echo string
    
    * pull the container first
    
    * chore: build master too
    
    * chore: build master too
    
    * fix: create the folder
    
    - add the badge too
    
    * fix: add the e2e/results folder

--- example/example.component.ts
+++ example/example.component.ts
@@ -1,9 +1,10 @@
-import { Router } from "@the/router";
-import { Just } from "maybe";
-// tslint:disable-next-line:quotemark
-import { Service } from "./service";
+import { Router } from '@the/router';
+import { Just } from 'maybe';
+import { Service } from './service';
 export class ExampleComponent {
   constructor(service: Service, router: Router, just: Just) {}
 
   myMethod() {}
+
+  mySecondMethod() {}
 }
---
+++ example/run-plus.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# kill the script if a command fails
+set -e
+
+# store
+#EXAMPLE_FOLDER set by live-test.sh
+APP_DIR=$PWD
+# stops the prompt which blocks the rest of the commands
+export NG_CLI_ANALYTICS=false
+
+
+# install the tarballed version of scuri
+npm i ./example/scuri.next.tgz 
+# use headless chrome in karma config 
+node ./switch-to-chrome-headless.js ./karma.conf.js
+node ./switch-to-chrome-headless.js ./src/karma.conf.js
+# add autospy to paths and create the file
+node ./add-auto-spy-to-path.js ./tsconfig.json
+npx ng g scuri:autospy 
+
+# create spec classic (no templates)
+npx ng g scuri:spec --name src/app/other/other.component.ts
+
+# update spec classic (no templates)
+
+# create a test with a template
+npx ng g scuri:spec --name src/app/my-com/my-com.component.ts --class-template './example/__specFileName__.template'
+
+# update a test with a template and template-functions
+cp -f example/to-update.componen*.ts ./src/app/
+npx ng g scuri:spec --name ./src/app/to-update.component.ts --class-template './example/__normalizedName__.custom.spec.ts.template' --update
+
+#npm test
+mkdir ./example/results
+cp -f src/app/*.spec.ts src/app/**/*.spec.ts ./example/results
