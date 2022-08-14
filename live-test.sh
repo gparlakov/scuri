abf4ac84d325d0441291fc1063f3e11ac0e21b12
Georgi Parlakov <gparlakov@gmail.com>
2022-08-14 22:55:14 +0300

    Feat/test dist with ng 14 container from scuri examples (#157)
    * chore: bump version
    
    * feat: live test with container
    
    * feat: add live test
    
    - live-test.sh
      - packs the npm package locally in a tarball and copies it into the ./example folder
      - gets the workdir of the image
      - starts the docker container and passes in the /example folder (as volume) in the angular app folder
      - supports DEBUG_LIVE_TEST for live debugging of the container
      - runs ./example/run-plus.sh in the workdir of the started container
    - run-plus.sh
      - installs the local scuri via the tarbal
      - updates karma.conf.js for running chrome headless
      - creates autospy (and adds it to the tsconfg.json path)
      - creates my-com.component.ts using a class template
      - updates one component using a class template function
      -  creates other.component with an observable and promise method
      -  add a jest suite to snapshot and test the results
    
    * echo docker
    
    * debug pipelines
    
    * debug azdo: echo string
    
    * pull the container first
    
    * chore: build master too
    
    * chore: build master too
    
    * fix: create the folder
    
    - add the badge too
    
    * fix: add the e2e/results folder

---
+++ live-test.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#do not kill the script if a command fails
+rm -rf dist
+set -e
+npm run build
+
+mv $(npm pack) ./example/scuri.next.tgz
+docker pull gparlakov/scuri:angular-14-app-v2
+APP_DIR="$(docker inspect --format='{{.Config.WorkingDir}}' gparlakov/scuri:angular-14-app-v2)"
+
+export EXAMPLE_FOLDER='example'
+
+if test -z "${DEBUG_LIVE_TEST}";  then
+  FLAGS='-t --rm'
+  ENTRYPOINT=$APP_DIR/$EXAMPLE_FOLDER/run-plus.sh
+else
+  FLAGS='-it'
+  ENTRYPOINT=/bin/sh
+fi
+
+echo 'docker run $FLAGS -v $(pwd)/example:$APP_DIR/example --entrypoint $ENTRYPOINT gparlakov/scuri:angular-14-app-v2'
+
+docker run $FLAGS -v $(pwd)/example:$APP_DIR/example --entrypoint $ENTRYPOINT gparlakov/scuri:angular-14-app-v2
+
+cp ./example/results/*.* ./e2e/results
+npx jest --roots=./e2e -- ./e2e/check-results.spec.ts 
