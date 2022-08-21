dc12a191f7eda82fcfb0dcea264fe37def1b22fa
Georgi Parlakov <gparlakov@gmail.com>
2022-08-21 19:44:39 +0000

    fix: indentation and leading space

--- tests/spec/spec-update.add-observable-and-promise-spy-return.spec.ts
+++ tests/spec/spec-update.add-observable-and-promise-spy-return.spec.ts
@@ -1,15 +1,11 @@
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import {
-    collectionPath,
-    getTestFile,
-    getTestFileContents
-} from './common';
+import { collectionPath, getTestFile, getTestFileContents } from './common';
 
 describe('spec for a class with a method calling a dependency method', () => {
     it('should add EMPTY for the dep method returning an observable', async () => {
         // arrange
-        const depsCallsReturnTypesFile = getTestFile('deps-calls-with-return-types.ts')
+        const depsCallsReturnTypesFile = getTestFile('deps-calls-with-return-types.ts');
         const specFileName = depsCallsReturnTypesFile.replace('.ts', '.spec.ts');
         const tree = Tree.empty();
         tree.create(depsCallsReturnTypesFile, getTestFileContents(depsCallsReturnTypesFile));
@@ -24,8 +20,44 @@ describe('spec for a class with a method calling a dependency method', () => {
         // assert
         const specFile = result!.readContent(depsCallsReturnTypesFile.replace('.ts', '.spec.ts'));
         expect(specFile).toBeDefined();
-        expect(specFile).toMatch('service.observableReturning.and.returnValue(EMPTY)')
-        expect(specFile).toMatch('service.promiseReturning.and.returnValue(new Promise(res => {}))');
+        expect(specFile).toMatchInlineSnapshot(`
+            "import { ServiceWithMethods } from './deps-calls-with-return-types.dependency';
+            import { ExampleComponent } from './deps-calls-with-return-types';
+            import { autoSpy } from 'autoSpy';
+
+            describe('ExampleComponent', () => {
+                it('when aMethod is called it should', () => {
+                    // arrange
+                    const { build } = setup().default();
+                    const e = build();
+                    // act
+                    e.aMethod();
+                    // assert
+                    // expect(e).toEqual
+                });
+
+              
+            });
+
+            function setup() {
+
+              const service = autoSpy(ServiceWithMethods);
+              service.observableReturning.and.returnValue(EMPTY);
+              service.promiseReturning.and.returnValue(new Promise(res => {}));
+              const builder = {
+                service,
+                default() {
+                  return builder;
+                },
+                build() {
+                  return new ExampleComponent(service);
+                }
+              };
+
+              return builder;
+            }
+            "
+        `);
     });
 
     it('should add EMPTY for the dep method returning an observable when dependency is already part of the spec', async () => {
@@ -45,7 +77,40 @@ describe('spec for a class with a method calling a dependency method', () => {
         // assert
         const specFile = result!.readContent(fileName.replace('.ts', '.spec.ts'));
         expect(specFile).toBeDefined();
-        expect(specFile).toMatch('service.observableReturning.and.returnValue(EMPTY)')
-        expect(specFile).toMatch('service.promiseReturning.and.returnValue(new Promise(res => {}))');
+        expect(specFile).toMatchInlineSnapshot(`
+            "import { ServiceWithMethods } from './deps-calls-with-return-types.dependency';
+            import { ExampleComponent } from './deps-calls-with-return-types';
+            import { autoSpy } from 'autoSpy';
+
+            describe('ExampleComponent', () => {
+                it('when aMethod is called it should', () => {
+                    // arrange
+                    const { build } = setup().default();
+                    const e = build();
+                    // act
+                    e.aMethod();
+                    // assert
+                    // expect(e).toEqual
+                });
+            });
+
+            function setup() {
+              const service = autoSpy(ServiceWithMethods);
+              service.observableReturning.and.returnValue(EMPTY);
+              service.promiseReturning.and.returnValue(new Promise(res => {}));
+              
+              const builder = {
+                default() {
+                  return builder;
+                },
+                build() {
+                  return new ExampleComponent(service);
+                }
+              };
+
+              return builder;
+            }
+            "
+        `);
     });
 });
--- tests/spec/test-data/deps-calls-with-return-types-dep-included.spec.ts
+++ tests/spec/test-data/deps-calls-with-return-types-dep-included.spec.ts
@@ -3,8 +3,15 @@ import { ExampleComponent } from './deps-calls-with-return-types';
 import { autoSpy } from 'autoSpy';
 
 describe('ExampleComponent', () => {
-
-  
+    it('when aMethod is called it should', () => {
+        // arrange
+        const { build } = setup().default();
+        const e = build();
+        // act
+        e.aMethod();
+        // assert
+        // expect(e).toEqual
+    });
 });
 
 function setup() {
