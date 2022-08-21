5e32091d998ebf3cdf7ce570786bf5cd6329977d
Georgi Parlakov <gparlakov@gmail.com>
2022-08-21 20:07:51 +0000

    chore: update e2e

--- example/run-plus.sh
+++ example/run-plus.sh
@@ -10,13 +10,13 @@ export NG_CLI_ANALYTICS=false
 
 
 # install the tarballed version of scuri
-npm i ./example/scuri.next.tgz 
-# use headless chrome in karma config 
+npm i ./example/scuri.next.tgz
+# use headless chrome in karma config
 node ./switch-to-chrome-headless.js ./karma.conf.js
 node ./switch-to-chrome-headless.js ./src/karma.conf.js
 # add autospy to paths and create the file
 node ./add-auto-spy-to-path.js ./tsconfig.json
-npx ng g scuri:autospy 
+npx ng g scuri:autospy
 
 # create spec classic (no templates)
 npx ng g scuri:spec --name src/app/other/other.component.ts
@@ -30,6 +30,6 @@ npx ng g scuri:spec --name src/app/my-com/my-com.component.ts --class-template '
 cp -f example/to-update.componen*.ts ./src/app/
 npx ng g scuri:spec --name ./src/app/to-update.component.ts --class-template './example/__normalizedName__.custom.spec.ts.template' --update
 
-#npm test
-mkdir ./example/results
+# copy results for external e2e test to run
+mkdir ./example/results || true
 cp -f src/app/*.spec.ts src/app/**/*.spec.ts ./example/results
