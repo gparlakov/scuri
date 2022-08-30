e84f3dc7bf696e5c45311ccb9862f7c8270dea34
Georgi Parlakov <gparlakov@gmail.com>
2022-08-30 14:24:08 +0000

    chore: push in private repo for test

---
+++ live-test-local.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#do not kill the script if a command fails
+
+rm -rf dist
+rm e2e/results/*.spec.ts
+rm -rf example/results
+
+set -e
+npm run build
+
+# package for local install
+mv $(npm pack) ./example/scuri.next.tgz
+
+docker pull gparlakov/scuri:angular-14-app-v2
+APP_DIR="$(docker inspect --format='{{.Config.WorkingDir}}' gparlakov/scuri:angular-14-app-v2)"
+
+export EXAMPLE_FOLDER='example'
+
+# support DEBUG_LIVE_TEST live-test
+if test -z "${DEBUG_LIVE_TEST}";  then
+  FLAGS='-t --rm'
+  ENTRYPOINT=$APP_DIR/$EXAMPLE_FOLDER/run-plus.sh
+else
+  FLAGS='-it'
+  ENTRYPOINT=/bin/sh
+fi
+
+# what 
+echo 'docker run $FLAGS -v $(pwd)/example:$APP_DIR/example --entrypoint $ENTRYPOINT gparlakov/scuri:angular-14-app-v2'
+
+# /example volume to access local files and then take the results from running the command 
+docker run $FLAGS -v $(pwd)/example:$APP_DIR/example --entrypoint $ENTRYPOINT gparlakov/scuri:angular-14-app-v2
+
+# test the results 
+cp ./example/results/*.* ./e2e/results
+npx jest --roots=./e2e -- ./e2e/check-results.spec.ts 
--- live-test.sh
+++ live-test.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-#do not kill the script if a command fails
 rm -rf dist
+rm e2e/results/*.spec.ts
+rm -rf example/results
+# in case it's running still
+docker-compose -f ./.verdaccio/docker-compose.yml down
+
+#kill the script if a command fails
 set -e
 npm run build
 
-mv $(npm pack) ./example/scuri.next.tgz
+# start the local registry
+
+sudo chown -R 10001:65533 $PWD/.verdaccio
+docker-compose -f ./.verdaccio/docker-compose.yml up -d
+# publish to local registry
+npm_config_registry=http://localhost:4873/ npm publish
+
+
 docker pull gparlakov/scuri:angular-14-app-v2
 APP_DIR="$(docker inspect --format='{{.Config.WorkingDir}}' gparlakov/scuri:angular-14-app-v2)"
 
@@ -19,9 +31,10 @@ else
   ENTRYPOINT=/bin/sh
 fi
 
-echo 'docker run $FLAGS -v $(pwd)/example:$APP_DIR/example --entrypoint $ENTRYPOINT gparlakov/scuri:angular-14-app-v2'
-
-docker run $FLAGS -v $(pwd)/example:$APP_DIR/example --entrypoint $ENTRYPOINT gparlakov/scuri:angular-14-app-v2
+docker run $FLAGS -v $(pwd)/example:$APP_DIR/example --entrypoint $ENTRYPOINT --net=host -e npm_config_registry=http://localhost:4873/ gparlakov/scuri:angular-14-app-v2
 
 cp ./example/results/*.* ./e2e/results
-npx jest --roots=./e2e -- ./e2e/check-results.spec.ts 
+npx jest --roots=./e2e -- ./e2e/check-results.spec.ts
+
+# stop local registry
+docker-compose -f ./.verdaccio/docker-compose.yml down
