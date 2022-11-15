1135d58a6bfc17a38247a87bdb1cb857c83f0439
Georgi Parlakov <gparlakov@gmail.com>
2022-11-15 11:24:42 +0200

    feat: support ts config extends

--- tests/spec/all.detect-testing-framework.spec.ts
+++ tests/spec/all.detect-testing-framework.spec.ts
@@ -2,7 +2,7 @@ import { LogEntry } from '@angular-devkit/core/src/logger';
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import { filter } from 'rxjs/operators';
-import { collectionPath, getTestFile, getTestFileContents } from './common';
+import { collectionPath, getTestFile, getTestFileContents, setupBase } from './common';
 import { getSpecFilePathName } from '../../src/common/get-spec-file-name';
 
 describe('Framework test runner config', () => {
@@ -101,48 +101,7 @@ describe('Framework test runner config', () => {
 });
 
 function setup() {
-    const tree = Tree.empty();
     const testFilesFolder = 'all.detect-testing-framework';
-    const defaultFileName = getTestFile(`${testFilesFolder}/component.ts`);
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const builder = {
-        testFilesFolder,
-        defaultFileName,
-        testFileName: getSpecFilePathName(defaultFileName),
-        tree,
-        runner,
-        log(o?: { map?: (l: LogEntry) => unknown; filter?: string | ((l: LogEntry) => boolean) }) {
-            const mapFn = typeof o?.map === 'function' ? o.map : (m: LogEntry) => m;
-            const filterFn =
-                typeof o?.filter === 'function'
-                    ? o.filter
-                    : typeof o?.filter === 'string'
-                    ? (m: LogEntry) =>
-                          m.name.includes(o.filter as string) ||
-                          m.message.includes(o.filter as string)
-                    : () => true;
-
-            const c = console; // hide from tslint
-            runner.logger.pipe(filter((f) => filterFn(f))).subscribe((l) => c.log(mapFn(l)));
-
-            return builder;
-        },
-        add(name: string, contents: string) {
-            tree.create(name, contents);
-            return builder;
-        },
-        default() {
-            return builder.add(defaultFileName, getTestFileContents(defaultFileName));
-        },
-        build() {
-            return tree;
-        },
-        async run(o?: { framework?: string }) {
-            return runner
-                .runSchematicAsync('spec', { ...o, name: defaultFileName }, tree)
-                .toPromise();
-        },
-    };
-
-    return builder;
+    const defaultFileName = 'component.ts';
+    return setupBase(testFilesFolder, defaultFileName)
 }
