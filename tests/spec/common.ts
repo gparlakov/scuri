ac04244a7cb92875a9922a2d6b695bfb4c93c627
Georgi Parlakov <gparlakov@gmail.com>
2023-03-11 14:47:32 +0200

    chore(tests): fix line split issue

--- tests/spec/common.ts
+++ tests/spec/common.ts
@@ -1,16 +1,15 @@
 import { join } from 'path';
 import { readFileSync } from 'fs';
-import { EOL } from 'os';
 
 export const collectionPath = join(__dirname, '../../collection.json');
 
-
 export const getTestFile = (fileName: string) => join(__dirname, './test-data', fileName);
-export const getTestFileContents = (fullFilePath: string) => readFileSync(fullFilePath).toString('utf8');
+export const getTestFileContents = (fullFilePath: string) =>
+    readFileSync(fullFilePath).toString('utf8');
 
 export const depsCallsReturnTypesFile = getTestFile('deps-calls-with-return-types.ts');
 export const depsCallsReturnTypesFileContents = () => getTestFileContents(depsCallsReturnTypesFile);
 
 export function splitLines(s: string): string[] {
-    return typeof s === 'string' ? s.replace(EOL, '\n').split('\n') : [];
+    return typeof s === 'string' ? s.split(/\n\r|\n|\r\n/) : [];
 }
--- tests/spec/spec-create.add-observable-and-promise-spy-return.spec.ts
+++ tests/spec/spec-create.add-observable-and-promise-spy-return.spec.ts
@@ -1,6 +1,5 @@
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import { filter } from 'rxjs/operators';
 import {
     collectionPath,
     depsCallsReturnTypesFile,
