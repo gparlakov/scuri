8905aab2829caa28c6b52d3932facc6ca1382a3b
Georgi Parlakov <gparlakov@gmail.com>
2023-03-25 14:27:11 +0200

    chore: update spec-update.by-giving-spec-name.spec.ts
    spec-update.by-giving-spec-name.spec.ts

--- tests/spec/spec-update.by-giving-spec-name.spec.ts
+++ tests/spec/spec-update.by-giving-spec-name.spec.ts
@@ -1,51 +1,39 @@
+import { LogEntry } from '@angular-devkit/core/src/logger';
 import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import { filter } from 'rxjs/operators';
-import { collectionPath } from './common';
+import { setupBase } from './common';
+
+
+const folder = 'update-by-giving-spec-name';
+const file = 'c.ts';
 
 describe('Calling update and passing the spec file in --name ', () => {
     let tree = Tree.empty();
 
     beforeEach(() => {
         tree = Tree.empty();
-        tree.create(
-            'c.ts',
-            `import { LogService, BDep } from '@angular/core';
-
-            export class C  {
-                constructor(
-                    private bDep: bDep,
-                    private logger: LogService
-                ) {}
-            `
-        );
-
-        tree.create(
-            'c.spec.ts',
-            `import { bDep } from '@angular/core';
-
-            describe('C', () => {
-            });`
-        );
     });
 
     it('should work and not throw errors', async  () => {
         // arrange
-        const runner = new SchematicTestRunner('schematics', collectionPath);
+        const { run, fullFileName, add, testFileName, letLogger } = setupBase(folder, file);
+        add(fullFileName);
+        add(testFileName);
         // act
-        const errors = [];
-        runner.logger.pipe(filter(v => v.level === 'error')).subscribe(v => errors.push(v));
-        await runner.runSchematicAsync('spec', { name: './c.spec.ts', update: true }, tree).toPromise();
+        const errors:LogEntry[] = [];
+        letLogger(logger => logger.pipe(filter(v => v.level === 'error')).subscribe(v => errors.push(v)));
+        await run({ name: fullFileName, update: true }).catch(e => fail('Failing on purpose: There should not be any errors here'));
         // assert
         expect(errors.length).toBe(0);
     });
 
     it('should update the spec file', async  () => {
-        // arrange
-        const runner = new SchematicTestRunner('schematics', collectionPath);
+        const { run, fullFileName, add, testFileName } = setupBase(folder, file);
+        add(fullFileName);
+        add(testFileName)
         // act
-        const result = await runner.runSchematicAsync('spec', { name: './c.spec.ts', update: true }, tree).toPromise();
-        const contents = result.readContent('./c.spec.ts');
+        const result = await run({ name: fullFileName, update: true });
+        const contents = result.readContent(testFileName);
         // assert
         expect(contents).toContain('const logger = autoSpy(LogService);');
         expect(contents).toContain('function setup() {');
