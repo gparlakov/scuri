9164e1fd56360e8216ff4c49ed6270f7f8906bd5
Georgi Parlakov <gparlakov@gmail.com>
2023-03-25 14:43:30 +0200

    chore: spec-update.dont-add-comma.spec.ts
    spec-update.dont-add-comma.spec.ts

---
+++ tests/spec/test-data/update-no-comma/c.spec.ts
@@ -0,0 +1,20 @@
+import { bDep } from '@angular/core';
+
+describe('C', () => {
+});
+
+function setup() {
+    const bDep = autoSpy(bDep);
+    const logger = autoSpy(LogService);
+    const builder = {
+        bDep,
+        logger,
+        default() {
+            return builder;
+        },
+        build() {
+            return new C(bDep, logger);
+        }
+    }
+    return builder;
+}
---
+++ tests/spec/test-data/update-no-comma/c.ts
@@ -0,0 +1,5 @@
+import { LogService, BDep } from '@angular/core';
+
+export class C {
+    constructor(private bDep: bDep, private logger: LogService) {}
+}
