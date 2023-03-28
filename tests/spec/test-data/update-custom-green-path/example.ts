f9b99201a81babf4fb10b225cda709dd8616eab1
Georgi Parlakov <gparlakov@gmail.com>
2023-03-28 09:51:06 +0300

    chore: update update-custom-green-path.spec.ts

---
+++ tests/spec/test-data/update-custom-green-path/example.ts
@@ -0,0 +1,12 @@
+import { Router } from "@the/router";
+import { Just } from "maybe";
+import { Service } from "./service";
+export class ExampleComponent {
+    constructor(service: Service, router: Router, just: Just) {}
+
+    myMethod() {}
+
+    yourMethod() {}
+
+    theirMethod() {}
+}
