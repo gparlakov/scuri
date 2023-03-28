15372c57157566d020f79a5bf111d1b5b88d7068
Georgi Parlakov <gparlakov@gmail.com>
2023-03-28 09:51:06 +0300

    chore: update update-custom-green-path.spec.ts

--- tests/update-custom/update-custom-green-path.spec.ts
+++ tests/update-custom/update-custom-green-path.spec.ts
@@ -1,138 +1,91 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import { Options } from '../../src/update-custom/index';
-import { collectionPath } from '../spec/common';
+import { setupBase, splitLines } from '../spec/common';
 
 describe('update-custom', () => {
-    let tree: Tree;
     const name = 'example.ts';
-    const specFileName = 'example.spec.ts';
+    const folder = 'update-custom-green-path';
     const classTemplate = '__specFileName__.template';
 
-    beforeEach(() => {
-        tree = Tree.empty();
-
-        tree.create(name, fileContent());
-        tree.create(specFileName, specFileContent());
-        tree.create(classTemplate, template());
-    });
-
     it('should add new and not repeat existing lines', async () => {
-        const runner = new SchematicTestRunner('schematics', collectionPath);
-        const treeAfter = await runner
-            .runSchematicAsync('update-custom', <Options>{ name, classTemplate }, tree)
-            .toPromise();
-        const result = treeAfter?.read(specFileName)?.toString('utf8').split(/\r\n|\r|\n/g)!;
-        expect(treeAfter.files.length).toEqual(3);
+        // arrange
+        const { run, fullFileName, add, testFileName } = setupBase(folder, name);
+        add(fullFileName);
+        add(testFileName);
+        add(classTemplate, template());
+        // act
+        const r = await run({ name: fullFileName, classTemplate }, 'update-custom');
+
+        // assert
+        const specFile = r!.readContent(testFileName);
+        const lines = splitLines(specFile);
+
         let i = 0;
 
-        expect(result[i++]).toEqual("import { MyDirective } from './directive';")
-        expect(result[i++]).toEqual("import { autoSpy, spyInject } from 'jasmine-auto-spies';")
-        expect(result[i++]).toEqual('')
-        expect(result[i++]).toEqual("describe('ExampleComponent', () => {")
-        expect(result[i++]).toEqual('    let serviceSpy: Service;')
-        expect(result[i++]).toEqual('    let routerSpy: Router;')
-        expect(result[i++]).toEqual('    let justSpy: Just;')
-        expect(result[i++]).toEqual('    // scuri:lets')
-        expect(result[i++]).toEqual('')
-        expect(result[i++]).toEqual('    beforeEach(')
-        expect(result[i++]).toEqual('        waitForAsync(() => {')
-        expect(result[i++]).toEqual('            TestBed.configureTestingModule({')
-        expect(result[i++]).toEqual('                providers: [')
-        expect(result[i++]).toEqual('                    MyDirective,')
-        expect(result[i++]).toEqual("                    { provide: Service, useClass: autoSpy(Service, 'Service') },")
-        expect(result[i++]).toEqual("                    { provide: Router, useClass: autoSpy(Router, 'Router') },")
-        expect(result[i++]).toEqual("                    { provide: Just, useClass: autoSpy(Just, 'Just') },")
-        expect(result[i++]).toEqual('                    // scuri:injectables')
-        expect(result[i++]).toEqual('                ]')
-        expect(result[i++]).toEqual('            });')
-        expect(result[i++]).toEqual('')
-        expect(result[i++]).toEqual('            directive = TestBed.inject(MyDirective);')
-        expect(result[i++]).toEqual('            serviceSpy = spyInject<Service>(TestBed.inject(Service));')
-        expect(result[i++]).toEqual('            routerSpy = spyInject<Router>(TestBed.inject(Router));')
-        expect(result[i++]).toEqual('            justSpy = spyInject<Just>(TestBed.inject(Just));')
-        expect(result[i++]).toEqual('            // scuri:get-instances')
-        expect(result[i++]).toEqual('        })')
-        expect(result[i++]).toEqual('    );')
-        expect(result[i++]).toEqual('')
-        expect(result[i++]).toEqual("    it('when myMethod is called it should', () => {")
-        expect(result[i++]).toEqual('        // arrange')
-        expect(result[i++]).toEqual('        // act')
-        expect(result[i++]).toEqual('        e.myMethod();')
-        expect(result[i++]).toEqual('        // assert')
-        expect(result[i++]).toEqual('        // expect(e).toEqual')
-        expect(result[i++]).toEqual('    });')
-        expect(result[i++]).toEqual('')
-        expect(result[i++]).toEqual("    it('when yourMethod is called it should', () => {")
-        expect(result[i++]).toEqual('        // arrange')
-        expect(result[i++]).toEqual('        // act')
-        expect(result[i++]).toEqual('        e.yourMethod();')
-        expect(result[i++]).toEqual('        // assert')
-        expect(result[i++]).toEqual('        // expect(e).toEqual')
-        expect(result[i++]).toEqual('    });')
-        expect(result[i++]).toEqual("    it('when theirMethod is called it should', () => {")
-        expect(result[i++]).toEqual('        // arrange')
-        expect(result[i++]).toEqual('        // act')
-        expect(result[i++]).toEqual('        e.theirMethod();')
-        expect(result[i++]).toEqual('        // assert')
-        expect(result[i++]).toEqual('        // expect(e).toEqual')
-        expect(result[i++]).toEqual('    });')
-        expect(result[i++]).toEqual('    ')
-        expect(result[i++]).toEqual('    // scuri:methods')
-        expect(result[i++]).toEqual('});')
+        expect(lines[i++]).toEqual("import { MyDirective } from './directive';");
+        expect(lines[i++]).toEqual("import { autoSpy, spyInject } from 'jasmine-auto-spies';");
+        expect(lines[i++]).toEqual('');
+        expect(lines[i++]).toEqual("describe('ExampleComponent', () => {");
+        expect(lines[i++]).toEqual('    let serviceSpy: Service;');
+        expect(lines[i++]).toEqual('    let routerSpy: Router;');
+        expect(lines[i++]).toEqual('    let justSpy: Just;');
+        expect(lines[i++]).toEqual('    // scuri:lets');
+        expect(lines[i++]).toEqual('');
+        expect(lines[i++]).toEqual('    beforeEach(');
+        expect(lines[i++]).toEqual('        waitForAsync(() => {');
+        expect(lines[i++]).toEqual('            TestBed.configureTestingModule({');
+        expect(lines[i++]).toEqual('                providers: [');
+        expect(lines[i++]).toEqual('                    MyDirective,');
+        expect(lines[i++]).toEqual(
+            "                    { provide: Service, useClass: autoSpy(Service, 'Service') },"
+        );
+        expect(lines[i++]).toEqual(
+            "                    { provide: Router, useClass: autoSpy(Router, 'Router') },"
+        );
+        expect(lines[i++]).toEqual(
+            "                    { provide: Just, useClass: autoSpy(Just, 'Just') },"
+        );
+        expect(lines[i++]).toEqual('                    // scuri:injectables');
+        expect(lines[i++]).toEqual('                ]');
+        expect(lines[i++]).toEqual('            });');
+        expect(lines[i++]).toEqual('');
+        expect(lines[i++]).toEqual('            directive = TestBed.inject(MyDirective);');
+        expect(lines[i++]).toEqual(
+            '            serviceSpy = spyInject<Service>(TestBed.inject(Service));'
+        );
+        expect(lines[i++]).toEqual(
+            '            routerSpy = spyInject<Router>(TestBed.inject(Router));'
+        );
+        expect(lines[i++]).toEqual('            justSpy = spyInject<Just>(TestBed.inject(Just));');
+        expect(lines[i++]).toEqual('            // scuri:get-instances');
+        expect(lines[i++]).toEqual('        })');
+        expect(lines[i++]).toEqual('    );');
+        expect(lines[i++]).toEqual('');
+        expect(lines[i++]).toEqual("    it('when myMethod is called it should', () => {");
+        expect(lines[i++]).toEqual('        // arrange');
+        expect(lines[i++]).toEqual('        // act');
+        expect(lines[i++]).toEqual('        e.myMethod();');
+        expect(lines[i++]).toEqual('        // assert');
+        expect(lines[i++]).toEqual('        // expect(e).toEqual');
+        expect(lines[i++]).toEqual('    });');
+        expect(lines[i++]).toEqual('');
+        expect(lines[i++]).toEqual("    it('when yourMethod is called it should', () => {");
+        expect(lines[i++]).toEqual('        // arrange');
+        expect(lines[i++]).toEqual('        // act');
+        expect(lines[i++]).toEqual('        e.yourMethod();');
+        expect(lines[i++]).toEqual('        // assert');
+        expect(lines[i++]).toEqual('        // expect(e).toEqual');
+        expect(lines[i++]).toEqual('    });');
+        expect(lines[i++]).toEqual("    it('when theirMethod is called it should', () => {");
+        expect(lines[i++]).toEqual('        // arrange');
+        expect(lines[i++]).toEqual('        // act');
+        expect(lines[i++]).toEqual('        e.theirMethod();');
+        expect(lines[i++]).toEqual('        // assert');
+        expect(lines[i++]).toEqual('        // expect(e).toEqual');
+        expect(lines[i++]).toEqual('    });');
+        expect(lines[i++]).toEqual('    ');
+        expect(lines[i++]).toEqual('    // scuri:methods');
+        expect(lines[i++]).toEqual('});');
     });
 });
-
-function fileContent(): string {
-    return `import { Router } from "@the/router";
-import { Just } from "maybe";
-import { Service } from "./service";
-export class ExampleComponent {
-    constructor(service: Service, router: Router, just: Just) {}
-
-    myMethod() {}
-
-    yourMethod() {}
-
-    theirMethod() {}
-}
-`;
-}
-
-function specFileContent(): string {
-    return `import { MyDirective } from './directive';
-import { autoSpy, spyInject } from 'jasmine-auto-spies';
-
-describe('ExampleComponent', () => {
-    // scuri:lets
-
-    beforeEach(
-        waitForAsync(() => {
-            TestBed.configureTestingModule({
-                providers: [
-                    MyDirective,
-                    { provide: Service, useClass: autoSpy(Service, 'Service') },
-                    // scuri:injectables
-                ]
-            });
-
-            directive = TestBed.inject(MyDirective);
-            // scuri:get-instances
-        })
-    );
-
-    it('when myMethod is called it should', () => {
-        // arrange
-        // act
-        e.myMethod();
-        // assert
-        // expect(e).toEqual
-    });
-
-    // scuri:methods
-});
-`;
-}
 
 function template() {
     return `<% params.forEach(p => { if(p.importPath) {%>import { <%= p.type %> } from '<%= p.importPath %>';
