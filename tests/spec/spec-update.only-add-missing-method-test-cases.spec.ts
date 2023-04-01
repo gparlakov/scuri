9655010d182afaa74ff0e64fcb63388ad938712b
Georgi Parlakov <gparlakov@gmail.com>
2023-04-01 18:49:31 +0300

    chore: update to snapshot only add missing test cases

--- tests/spec/spec-update.only-add-missing-method-test-cases.spec.ts
+++ tests/spec/spec-update.only-add-missing-method-test-cases.spec.ts
@@ -1,5 +1,4 @@
 import { LogEntry } from '@angular-devkit/core/src/logger';
-import { Tree } from '@angular-devkit/schematics';
 import { filter } from 'rxjs/operators';
 import { setupBase } from './common';
 
@@ -7,23 +6,17 @@ const folder = 'only-add-missing-method-test-cases';
 const file = 'c.ts';
 
 describe('Calling update', () => {
-    let tree = Tree.empty();
-
-    beforeEach(() => {
-        tree = Tree.empty();
-    });
-
     it('should work and not throw errors', async () => {
         // arrange
         const { run, fullFileName, add, testFileName, letLogger } = setupBase(folder, file);
         add(fullFileName);
         add(testFileName)
         // act
-        await run({ name: fullFileName, update: false });
+        await run({ name: fullFileName, update: true });
         // act
         const errors: LogEntry[] = [];
         letLogger(logger => logger.pipe(filter((v) => v.level === 'error')).subscribe((v) => errors.push(v)));
-        await run({ name: './c.spec.ts', update: true }).catch(e => {
+        await run({ name: fullFileName, update: true }).catch(e => {
             fail('Failing the test on purpose: There should not be any errors thrown here. ')
         });
         // assert
@@ -34,34 +27,11 @@ describe('Calling update', () => {
         // arrange
         const { run, fullFileName, add, testFileName } = setupBase(folder, file);
         add(fullFileName);
-        add(testFileName)
+        add(testFileName);
         // act
-        const result = await run({ name: fullFileName, update: false });
+        const result = await run({ name: fullFileName, update: true });
         const contents = result.readContent(testFileName);
         // assert
-        expect(contents).toEqual(`import { bDep } from '@angular/core';
-
-describe('C', () => {
-    it('existing method one test', () => {
-        c.methodOne();
-    })
-    it('existing test case with name of methodTwo in the spec title', () => {
-        c.methodTwo();
-    })
-    it('just any name', () => {
-        c.methodThree();
-    })
-});
-function setup() {
-    const builder = {
-        default() {
-            return builder;
-        },
-        build() {
-            return new C();
-        }
-    }
-    return builder;
-}`);
+        expect(contents).toMatchSnapshot();
     });
 });
